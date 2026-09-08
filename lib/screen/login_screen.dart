389869c2ed38b1d5e7bc7e30e6734d1665a7b55a
Brandonn Roblero <le21080737@merida.tecnm.mx>
2026-09-08 13:49:57 -0600

    feat(login): add email and password inputs with visibility control

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -9,11 +9,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //CONTROL PARA MOSTRAR/OCULTAR CONTRASEÑA
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
+    //PARA OBTENER EL TAMAÑO DE LA PANTALLA
+    final Size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [Expanded(child: RiveAnimation.asset('assets/corto.riv'))],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                width: Size.width,
+                child: RiveAnimation.asset('assets/corto.riv'),
+              ),
+
+              //PARA SEPARAR WIDGETS
+              SizedBox(height: 15),
+              //CAMPO DE TEXTO DE EMAIL
+              TextField(
+                //PARA MSTRAR UN TIPO DE TECLADO
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15),
+              //CAMPO DE TEXTO DE contraseña
+              TextField(
+                //PARA MSTRAR UN TIPO DE TECLADO
+                obscureText: _obscureText,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
